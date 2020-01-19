5d8d1c62eb4919b27d3fa0ea37eea19278f3f0b5
Max <max@digitalscarcity.io>
2020-01-19 12:55:33 -0500

    updating deployment rules

---
+++ test_listen.js
@@ -0,0 +1,33 @@
+global.fetch = require('node-fetch')
+global.WebSocket = require('ws')
+
+const telosTrxStatus =  () =>{
+    const messageBody = {
+          "apikey": "test-api-key",
+          "event": "subscribe",
+          "type": "get_actions",
+          "data": {
+              "account": "eosio", 
+              "actions": ["transfer"]
+          }
+      }
+      const  socket = new WebSocket("wss://telos.spectrumeos.io/streaming")
+    
+      socket.onopen = () => {
+          socket.send(JSON.stringify(messageBody));
+      }
+  
+      socket.onmessage = (event) => {
+          console.log('Telos websocket:', JSON.parse(event.data))
+    }
+    socket.onclose = (event)=> {
+      console.log("Telos socket connection closed:" +event.data)
+    }
+    socket.onerror = function(error) {
+          console.log("Telos websocket got error: " +error.message)
+      }
+  
+}
+
+telosTrxStatus()
+
